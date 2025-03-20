ce6bc6baf77a2de812a4c521dea1ae7964dcead7
bharat1704 <bharatmali1704@gmail.com>
2025-03-20 13:00:00 +0530

    added array in get route

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const app = express();
 
 app.get("/", function(req,res){
 
-    res.json({
+    res.json([{
         name:"AWS",
-        email: "AWs@gmail.com"
-    })
+        email: "aws@gmail.com"
+    },
+    {name:"AWS2",
+    email:"aws2@gmail.com"
+}]
+)
 })
 
 app.listen(3000);
