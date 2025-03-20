0f5e02b61921dd23ef285abcee38132cb3ff7add
bharat1704 <bharatmali1704@gmail.com>
2025-03-20 11:00:24 +0530

    created index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express();
+
+app.get("", function(req,res){
+
+    res.json({
+        name:"Test",
+        email: "test@gmail.com"
+    })
+})
+
+app.listen(3000);
